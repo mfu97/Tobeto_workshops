8c15df0d1391169faa81f05b1d1648f5eab3e2d1
Fatih <mfu97@hotmail.com>
2023-11-01 01:55:09 +0300

    Workshop_Day2

---
+++ src/main/java/Workshop_Day2/Category.java
@@ -0,0 +1,17 @@
+package Workshop_Day2;
+
+public class Category {
+    private String categoryName;
+    public Category() {
+    }
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
---
+++ src/main/java/Workshop_Day2/Order.java
@@ -0,0 +1,16 @@
+package Workshop_Day2;
+
+public class Order {
+    private int orderNumber;
+    public Order() {
+    }
+    public Order(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+}
